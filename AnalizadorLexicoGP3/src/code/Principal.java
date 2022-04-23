5f456cceb3bbba8ccb7eb35f9b977bdec3044626
walyec <52383359+walyec@users.noreply.github.com>
2022-04-23 01:30:33 -0600

    Estetica de Codigo

--- AnalizadorLexicoGP3/src/code/Principal.java
+++ AnalizadorLexicoGP3/src/code/Principal.java
@@ -2,16 +2,17 @@ package code;
 
 import java.io.File;
 
-
 public class Principal {
+
     public static void main(String[] args) {
         String ruta = "C:/Users/walyn/OneDrive/Documentos/NetBeansProjects/AnalizadorLexicoGP3/src/code/Lexer.flex";
         generarLexer(ruta);
     }
-    public static void generarLexer(String ruta){
+
+    public static void generarLexer(String ruta) {
         File archivo = new File(ruta);
         JFlex.Main.generate(archivo);
-        
+
     }
-    
+
 }
